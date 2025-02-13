00a496b881468a0ec9606434c10847ecde1d0551
kanu-cast <karkay349@gmail.com>
2025-02-13 18:22:43 +0200

    migrations models and seeders

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,47 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
+// module.exports = {
+//   development: {
+//     username: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+//     database: process.env.DB_NAME,
+//     host: process.env.DB_HOST || "127.0.0.1",
+//     dialect: "postgres",
+//     schema: "public"
+//   }
+// };
+require("dotenv").config();
+
 module.exports = {
   development: {
     username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    host: process.env.DB_HOST || "127.0.0.1",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
     dialect: "postgres",
+    logging: false,
+    schema: "public"
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    dialect: "postgres",
+    logging: false,
+    schema: "public"
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    dialect: "postgres",
+    logging: false,
     schema: "public"
   }
 };
