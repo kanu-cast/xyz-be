abf8ca241ef7e5f5228e9ac513e55ab7daa0d1c3
kanu-cast <karkay349@gmail.com>
2025-02-13 18:22:43 +0200

    migrations models and seeders

--- src/database/seeders/20250213015728-demo-users.js
+++ src/database/seeders/20250213015728-demo-users.js
@@ -4,18 +4,18 @@ module.exports = {
     await queryInterface.bulkInsert("users", [
       {
         user_id: "b1a8a9cf-1234-5678-9101-abcdefabcdef", // Inventory Manager(Admin)
-        full_name: "Alice Johnson",
-        email: "alice@example.com",
-        password_hash: "hashedpassword123",
+        full_name: "Lilliane Uwase",
+        email: "admin@xyz.com",
+        password: "pass123",
         phone_number: "1234567890",
         role: "inventory manager",
         created_at: new Date()
       },
       {
         user_id: "c2b8a9cf-1234-5678-9101-abcdefabcdef", // Program Manager
-        full_name: "Bob Smith",
-        email: "bob@example.com",
-        password_hash: "hashedpassword456",
+        full_name: "Jean Eric",
+        email: "manager@xyz.com",
+        password: "pass123",
         phone_number: "0987654321",
         role: "program manager",
         created_at: new Date()
